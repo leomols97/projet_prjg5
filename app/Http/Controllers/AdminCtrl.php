17ba19716e6b4bfb17a82c2cf4f571d09ff59a7d
Andre Pereira <51999@etu.he2b.be>
2020-09-24 12:24:06 +0200

    ajout produit sur bd

--- app/Http/Controllers/AdminCtrl.php
+++ app/Http/Controllers/AdminCtrl.php
@@ -20,6 +20,12 @@ class AdminCtrl extends Controller {
     }
 
     public function addProduct() {
-
+        $product = new \App\Product;
+        $product->prod_id = htmlentities($_POST["prod_id"]);
+        $product->description = htmlentities($_POST["description"]);
+        $product->price = htmlentities($_POST["price"]);
+        $product->stock_qt = htmlentities($_POST["stock_qt"]);
+        $product->path = htmlentities($_POST["path"]);
+        $product->save();
     }
 }
