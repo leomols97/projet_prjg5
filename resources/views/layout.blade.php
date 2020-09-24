af32060d68f0d52be18aa55817a49836e889db2a
Léopold Mols <53212@student.he2b.be>
2020-09-24 09:34:48 +0200

    Début formulaire enregistement étudiant

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>AchatInformatique</title>
+    </head>
+    <body>
+        <header>
+            @yield('title')
+        </header>
+
+        <nav>
+
+        </nav>
+        <main>
+            @yield('content')
+        </main>
+        <footer>
+            <p>
+                51999 - André Pereira
+            </p>
+            <p>
+                53212 - Léopold Mols
+            </p>
+            <p>
+                PRJG5 - SRV
+            </p>
+        </footer>
+    </body>
+</html>
