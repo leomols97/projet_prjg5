1c6383a3c59524971c90515769a4a3a56f1b3c70
Andre Pereira <51999@etu.he2b.be>
2020-09-24 11:15:26 +0200

    Mise a jour

--- resources/views/administrateur.blade.php
+++ resources/views/administrateur.blade.php
@@ -1,16 +1,16 @@
 @extends('layout')
 
-@section('title', 'Page d'administrateur')
+@section('title')
+    <h1>Page d'Administrateur</h1>
+@stop
 
 @section('content')
-    <p>This is my body content.</p>
-
 <form name="createStudent">
         <label for="last_name">Nom de Famille:</label>
         <input type="text" name="last_name" value="Nom de l'étudiant"><br>
         <input type="text" name="first_name" value="Prénom de l'étudiant"><br>
         <input type="text" name="matricule" value="Numéro de l'étudiant"><br>
         <button type="submit">Enregistrer</button>
-    </form>
+</form>
 
 @stop
