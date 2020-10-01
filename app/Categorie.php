4c113db0ae3d1d60a40d05d154c5dd3b892788cf
Andre Pereira <51999@etu.he2b.be>
2020-10-01 10:59:41 +0200

    Merge

--- app/Categorie.php
+++ app/Categorie.php
@@ -6,17 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categorie extends Model
 {
-<<<<<<< HEAD
     public $timestamps = false;
-=======
-    public $timestamp = false;
->>>>>>> leopold
 
     protected $table = 'categories';
     protected $primaryKey = 'cat_id';
     protected $fillable = ['description'];
-<<<<<<< HEAD
-
-=======
->>>>>>> leopold
 }
